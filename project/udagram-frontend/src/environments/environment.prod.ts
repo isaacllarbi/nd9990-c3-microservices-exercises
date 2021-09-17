f72aefd914a586630d40c966496a43edb3cb5717
Isaac Larbi <isaacllarbi@gmail.com>
2021-09-17 15:55:10 +0000

    added travis.yml file

--- project/udagram-frontend/src/environments/environment.prod.ts
+++ project/udagram-frontend/src/environments/environment.prod.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://localhost:8080/api/v0'
+  apiHost: 'http://test-env-centos.theadvisorylab.net:8080/api/v0'
 };
 
 /*
